d9bc0b7bc2f5faaca93a9afe767f1f69e3cbc41e
francovici <k39997@frro.utn.edu.ar>
2024-02-25 12:26:54 -0300

    uploaded example.sql

---
+++ sql/example.sql
@@ -0,0 +1,8 @@
+INSERT INTO Companies VALUES (1,'Google','Search Engine');
+INSERT INTO Companies VALUES (1,'Google','Search Engine');
+INSERT INTO Companies
+VALUES (1,'Google','Search Engine');
+INSERT INTO Companies
+(ID, Name, Description)
+VALUES
+(1,'Google','Search Engine');
